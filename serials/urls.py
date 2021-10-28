a1d475582ce3e76d963543d76312167d6a0cb4d3
Ilya <ilya.kramt@gmail.com>
2021-10-28 18:03:33 +0300

    add new routes

--- serials/urls.py
+++ serials/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from serials.views import SerialsHomeView
+from serials.views import SerialsHomeView, about, contact, login, series_detail, show_category
 
 urlpatterns = [
     path('', SerialsHomeView.as_view(), name='home'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('series/<int:series_id>/', series_detail, name='series_detail'),
+    path('category/<int:category_id>/', show_category, name='category'),
 ]
