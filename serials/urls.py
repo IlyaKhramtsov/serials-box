caca9b80a481f851cb7446766093596af3ea338d
Ilya <ilya.kramt@gmail.com>
2021-10-28 19:55:30 +0300

    update urls: add slugs

--- serials/urls.py
+++ serials/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
-    path('series/<int:series_id>/', series_detail, name='series_detail'),
-    path('category/<int:category_id>/', show_category, name='category'),
+    path('series/<slug:series_slug>/', series_detail, name='series_detail'),
+    path('category/<slug:category_slug>/', show_category, name='category'),
 ]
