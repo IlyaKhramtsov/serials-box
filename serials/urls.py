b346a4933c2c691b156f99000352c59928fe1890
Ilya <ilya.kramt@gmail.com>
2021-10-29 15:41:32 +0300

    [upd] create new classes -> SeriesDetail, SerialsCategory

--- serials/urls.py
+++ serials/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from serials.views import SerialsHomeView, about, contact, login, series_detail, show_category
+from serials.views import SerialsHomeView, SerialsCategory, SeriesDetail, about, contact, login
 
 urlpatterns = [
     path('', SerialsHomeView.as_view(), name='home'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
-    path('series/<slug:series_slug>/', series_detail, name='series_detail'),
-    path('category/<slug:category_slug>/', show_category, name='category'),
+    path('series/<slug:series_slug>/', SeriesDetail.as_view(), name='series_detail'),
+    path('category/<slug:category_slug>/', SerialsCategory.as_view(), name='category'),
 ]
--- serials/views.py
+++ serials/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from serials.models import TVSeries
 
@@ -14,6 +14,22 @@ class SerialsHomeView(ListView):
         return TVSeries.objects.filter(is_published=True)
 
 
+class SeriesDetail(DetailView):
+    model = TVSeries
+    template_name = 'serials/series_detail.html'
+    slug_url_kwarg = 'series_slug'
+    context_object_name = 'series'
+
+
+class SerialsCategory(ListView):
+    model = TVSeries
+    template_name = 'serials/index.html'
+    context_object_name = 'serials'
+
+    def get_queryset(self):
+        return TVSeries.objects.filter(category__slug=self.kwargs['category_slug'], is_published=True)
+
+
 def about(request):
     return render(request, 'serials/about.html', {'title': 'О проекте'})
 
@@ -24,19 +40,3 @@ def contact(request):
 
 def login(request):
     return HttpResponse("Авторизация")
-
-
-def series_detail(request, series_slug):
-    series = get_object_or_404(TVSeries, slug=series_slug)
-    context = {
-        'series': series,
-    }
-    return render(request, 'serials/series_detail.html', context=context)
-
-
-def show_category(request, category_slug):
-    serials = TVSeries.objects.filter(category__slug=category_slug)
-    context = {
-        'serials': serials,
-    }
-    return render(request, 'serials/index.html', context=context)
