219a7c4b538b3d998cc43210cf3a7a0077825c7a
Ilya <ilya.kramt@gmail.com>
2021-10-30 18:56:49 +0300

    set admin site

--- serials/admin.py
+++ serials/admin.py
@@ -2,7 +2,37 @@ from django.contrib import admin
 
 from serials.models import Category, Crew, Genre, TVSeries
 
-admin.site.register(Category)
-admin.site.register(Crew)
-admin.site.register(Genre)
-admin.site.register(TVSeries)
+
+class TVSerialsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'poster', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'description')
+    list_editable = ('is_published',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class CrewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'photo')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Crew, CrewAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(TVSeries, TVSerialsAdmin)
