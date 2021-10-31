3e0a4f46f0a126319d9c3ec7816928db165eab2a
Ilya <ilya.kramt@gmail.com>
2021-10-31 18:03:58 +0300

    add absolute url to Crew model

--- serials/models.py
+++ serials/models.py
@@ -74,3 +74,6 @@ class Crew(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('crew_detail', kwargs={"crew_slug": self.slug})
