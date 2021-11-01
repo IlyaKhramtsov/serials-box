240bc331e17d8c5cead2d32e7a7782b476caba44
Ilya <ilya.kramt@gmail.com>
2021-11-01 18:55:43 +0300

    remove contact route to users app

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from users.views import LoginUser, RegisterUser, logout_user
+from users.views import ContactFormView, LoginUser, RegisterUser, logout_user
 
 urlpatterns = [
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
     path('register/', RegisterUser.as_view(), name='register'),
+    path('contact/', ContactFormView.as_view(), name='contact'),
 ]
