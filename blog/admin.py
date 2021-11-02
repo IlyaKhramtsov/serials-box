123600a86f246aa957503100a9b42de9a7c00b85
Ilya <ilya.kramt@gmail.com>
2021-11-02 16:01:04 +0300

    initial commit

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from blog.models import Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'time_create', 'photo', 'is_published')
+    prepopulated_fields = {'slug': ('title',)}
