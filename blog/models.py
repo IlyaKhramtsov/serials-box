c6a27f92ba61b207b877536ce8cfbc897992fc5c
Ilya <ilya.kramt@gmail.com>
2021-11-02 19:56:03 +0300

    add absolute url to Article model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Article(models.Model):
@@ -18,3 +19,7 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        """Get absolute URL for articles."""
+        return reverse('article_detail', kwargs={'article_slug': self.slug})
