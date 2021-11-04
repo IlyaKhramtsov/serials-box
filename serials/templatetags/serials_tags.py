0a3d9ece66b57842dad114e1d396ad2eca6665a6
Ilya <ilya.kramt@gmail.com>
2021-11-04 19:01:10 +0300

    add the most commented serials

--- serials/templatetags/serials_tags.py
+++ serials/templatetags/serials_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from serials.models import Category, TVSeries
 
@@ -13,5 +14,13 @@ def get_categories():
 
 @register.inclusion_tag('serials/tags/last_serials.html')
 def show_last_serials(count=5):
-    serials = TVSeries.objects.filter(is_published=True).order_by("-id")[:count]
-    return {"last_serials": serials}
+    """Show the latest added TV series."""
+    serials = TVSeries.objects.filter(is_published=True).order_by('-id')[:count]
+    return {'last_serials': serials}
+
+
+@register.inclusion_tag('serials/tags/most_commented_serials.html')
+def get_most_commented_serials(count=5):
+    """Show the most commented serials."""
+    serials = TVSeries.objects.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+    return {'most_commented_serials': serials}
