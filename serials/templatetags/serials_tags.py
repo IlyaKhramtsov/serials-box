bdc7bad33be5a086133b8089dc688a7a944c928f
Ilya <ilya.kramt@gmail.com>
2021-11-04 17:51:26 +0300

    add template tag show_last_serials

--- serials/templatetags/serials_tags.py
+++ serials/templatetags/serials_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from serials.models import Category
+from serials.models import Category, TVSeries
 
 register = template.Library()
 
@@ -9,3 +9,9 @@ register = template.Library()
 def get_categories():
     """Show all categories."""
     return Category.objects.all()
+
+
+@register.inclusion_tag('serials/tags/last_serials.html')
+def show_last_serials(count=5):
+    serials = TVSeries.objects.filter(is_published=True).order_by("-id")[:count]
+    return {"last_serials": serials}
