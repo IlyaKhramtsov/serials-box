a39408e2e9120c39723710b49b27e542d1aa37f2
Ilya <ilya.kramt@gmail.com>
2021-11-05 18:10:41 +0300

    add rendering active items on menu

--- serials/templatetags/serials_tags.py
+++ serials/templatetags/serials_tags.py
@@ -24,3 +24,11 @@ def get_most_commented_serials(count=5):
     """Show the most commented serials."""
     serials = TVSeries.objects.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
     return {'most_commented_serials': serials}
+
+
+@register.simple_tag(takes_context=True)
+def activate_on(context, name):
+    """Render one active item on menu."""
+    if context['request'].resolver_match.url_name == name:
+        return 'active'
+    return ''
