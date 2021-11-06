c10c9952da99269ef9b05a7df463a99ce14f8586
Ilya <ilya.kramt@gmail.com>
2021-11-06 19:34:50 +0300

    add user profile

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from users.models import Contact
+from users.models import Contact, Profile
 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('email', 'name')
     search_fields = ('email', 'name')
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'photo')
+
