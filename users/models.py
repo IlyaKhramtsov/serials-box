b911fd8e279313c52f96372d8fb01582fb41f4e7
Ilya <ilya.kramt@gmail.com>
2021-11-15 23:39:47 +0300

    add auto save user profile

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Contact(models.Model):
@@ -18,11 +20,22 @@ class Contact(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, verbose_name='Пользователь')
     bio = models.TextField(blank=True, verbose_name='Биография')
-    photo = models.ImageField(upload_to="users", verbose_name="Фото")
+    photo = models.ImageField(upload_to="users", null=True, blank=True, verbose_name="Фото")
+    birthday = models.DateField(null=True, blank=True, verbose_name='День рождения')
+    city = models.CharField(max_length=50, null=True, blank=True, verbose_name='Город')
 
     class Meta:
         verbose_name = "Профиль"
         verbose_name_plural = "Профили"
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
     def __str__(self):
         return str(self.user)
