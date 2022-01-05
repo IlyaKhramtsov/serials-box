91d5b42d21c96848ff15ff788edc46281c1b5d51
Ilya <ilya.kramt@gmail.com>
2022-01-05 16:22:07 +0300

    add tests to serials models

--- serials/tests/test_models.py
+++ serials/tests/test_models.py
@@ -4,21 +4,22 @@ from django.test import TestCase
 from serials.models import Category, Crew, Comment, Genre, TVSeries
 
 
-class SerialsModelsTest(TestCase):
+class TVSeriesModelTest(TestCase):
 
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
             'User1', 'user1@mail.com')
 
-        self.category = Category.objects.create(
+        cls.category = Category.objects.create(
             name='test category',
             slug='test-category'
         )
 
-        self.series = TVSeries.objects.create(
+        cls.series = TVSeries.objects.create(
             title='Scrubs',
             description='Scrubs description',
-            category=self.category,
+            category=cls.category,
             year=2001,
             slug='scrubs'
         )
@@ -46,23 +47,11 @@ class SerialsModelsTest(TestCase):
         self.assertEqual(self.series.year, 2001)
         self.assertEqual(self.series.category.name, 'test category')
         self.assertEqual(self.series.genres.get(id=1).name, self.genre.name)
-        self.assertEqual(self.series.actors.get(id=1).name, self.actor.name)
-        self.assertEqual(self.series.directors.get(id=2).name, self.director.name)
+        self.assertEqual(self.series.actors.get(id=2).name, self.actor.name)
+        self.assertEqual(self.series.directors.get(id=3).name, self.director.name)
 
     def test_serials_absolute_url(self):
         self.assertEqual(self.series.get_absolute_url(), '/series/scrubs/')
-
-    def test_comment_model(self):
-        comment = Comment.objects.create(
-            series=self.series,
-            author=self.user,
-            text='The best series'
-        )
-
-        self.assertEqual(self.user.comment_set.get(id=1).series, self.series)
-        self.assertEqual(self.user.comment_set.get(id=1).text, 'The best series')
-        self.assertEqual(comment.series.title, self.series.title)
-        self.assertEqual(comment.author.username, self.user.username)
 
     def test_series_in_favorites(self):
         """Tests that the series is in the user's favorites."""
@@ -71,3 +60,62 @@ class SerialsModelsTest(TestCase):
         self.assertTrue(self.series.favorite)
         self.assertEqual(self.user.favorite.get(title='Scrubs'), self.series)
         self.assertEqual(self.series.favorite.get(id=self.user.id), self.user)
+
+
+class GenreModelTest(TestCase):
+
+    def setUp(self):
+        self.genre = Genre(name='Test genre', slug='test-genre')
+
+    def test_genre_str_is_equal_to_name(self):
+        self.assertEqual(self.genre.__str__(), self.genre.name)
+
+
+class CommentModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            'User1', 'user1@mail.com')
+
+        category = Category.objects.create(
+            name='test category',
+            slug='test-category'
+        )
+
+        cls.series = TVSeries.objects.create(
+            title='Test series',
+            description='Test series description',
+            category=category,
+            year=2001,
+            slug='test-series'
+        )
+
+        cls.comment = Comment.objects.create(
+            series=cls.series,
+            author=cls.user,
+            text='The best series'
+        )
+
+    def test_comment_str(self):
+        expected_str = f'Комментарий от {self.comment.author} на {self.comment.series}'
+        self.assertEqual(self.comment.__str__(), expected_str)
+
+    def test_comment_listing(self):
+        self.assertEqual(self.user.comment_set.get(id=1).series, self.series)
+        self.assertEqual(self.user.comment_set.get(id=1).text, 'The best series')
+        self.assertEqual(self.comment.series.title, self.series.title)
+        self.assertEqual(self.comment.author.username, self.user.username)
+
+
+class CrewModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.crew = Crew.objects.create(name='test_actor', slug='test-actor')
+
+    def test_crew_str_is_equal_to_name(self):
+        self.assertEqual(self.crew.__str__(), self.crew.name)
+
+    def test_serials_absolute_url(self):
+        self.assertEqual(self.crew.get_absolute_url(), '/crew/test-actor/')
