492bb21e854f9386ca54e9ddc8353280ef69767f
Ilya <ilya.kramt@gmail.com>
2022-01-15 19:23:34 +0300

    add is_favorite to context in tv series

--- serials/views.py
+++ serials/views.py
@@ -34,6 +34,7 @@ class SeriesDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['comment_form'] = CommentForm()
+        context['is_favorite'] = self.object.favorite.filter(id=self.request.user.id).exists()
         return context
 
 
