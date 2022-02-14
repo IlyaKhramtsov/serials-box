44221ee43357806933cff5660a6b85c849e7ed18
Ilya <ilya.kramt@gmail.com>
2022-02-14 20:40:14 +0300

    fix multiple authentication error

--- users/views.py
+++ users/views.py
@@ -27,7 +27,7 @@ class UserRegisterView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
-        login(self.request, user)
+        login(self.request, user, backend='django.contrib.auth.backends.ModelBackend')
         return redirect('home')
 
 
