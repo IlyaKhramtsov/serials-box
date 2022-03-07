fa46d9406e77ce0ec5dc65cf97c5f814d0ae6c76
Ilya <ilya.kramt@gmail.com>
2022-03-07 22:18:45 +0300

    fix article model

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Article(models.Model):
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
     likes = models.ManyToManyField(
-        User, related_name="blog_articles", verbose_name="Лайки"
+        User, blank=True, related_name="blog_articles", verbose_name="Лайки"
     )
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
